a89cf9ef9e4064547f4f539b936bd84fa2638512
Motunrayo Adeneye <67984447+motuncoded@users.noreply.github.com>
2025-07-06 12:44:27 +0100

    add: error boundary

--- alx-rick-and-morty-app/components/ErrorBoundary.tsx
+++ alx-rick-and-morty-app/components/ErrorBoundary.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import * as Sentry from "@sentry/react";
 
 interface State {
   hasError: boolean;
@@ -19,9 +20,8 @@ class ErrorBoundary extends React.Component<ErrorBoundaryProps, State> {
   }
 
   componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
-    console.log({ error, errorInfo });
+    Sentry.captureException(error, { extra: errorInfo });
   }
-
   render() {
     if (this.state.hasError) {
       return (
